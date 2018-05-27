7a52e8a75e7d4d8c0ed9a30ebee6f32355d6d05c
kunal mittal <kanumittal99@gmail.com>
2018-05-27 14:43:11 +0530

    Initial commit

---
+++ express/server.js
@@ -0,0 +1,38 @@
+const express=require('express');
+const hbs=require('hbs');
+var app=express();
+// set app related parameters.
+app.set('view engine','hbs');
+hbs.registerPartials('../views/partials');
+app.use(express.static('../public'));
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    console.log(`${now}`);
+    next();
+});
+app.get('/',(req,res)=>{
+    res.send('<h1>Hello express</h1>');
+});
+app.get('/about',(req,res)=>{
+    //res.send('<h1>Hello express</h1>');
+    /*res.send({
+        name:'Kunal',
+        age:18
+    });*/
+    res.render('../../views/about.hbs',{
+        auther:'Kunal Mittal',
+        age:18,
+        currentYear:new Date().getFullYear()
+    });
+});
+app.post('/about',(req,res)=>{
+    var obj={
+        name:'Kunal',
+        age:20,
+        location:'Jaipur'
+    };
+    res.send(JSON.stringify(obj,undefined,2));
+});
+app.listen(3000,()=>{
+    console.log('Server Started');
+});
